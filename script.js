0f76595c301a9af017dc02efc85fa4b1090dcd76
cardo1011 <117056167+cardo1011@users.noreply.github.com>
2024-01-15 12:00:56 -0800

    base CSS layed out and JS vars created

---
+++ script.js
@@ -0,0 +1,8 @@
+const xp = 0;
+const health = 100;
+const gold = 50;
+const currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+const button1 = document.querySelector("#button1");
